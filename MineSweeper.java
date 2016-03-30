54ab21106e17b02b363f7463299cb05005554803
Margarita <margarita@localhost.localdomain>
2016-03-30 22:35:46 +0300

    method for recursive show conncted zeroes. Need to fixed

--- MineSweeper.java
+++ MineSweeper.java
@@ -84,12 +84,100 @@ public class MineSweeper implements ActionListener {
                     if (counts[i][j] != MINE) {
                         buttons[i][j].setText(counts[i][j] + "");
                         buttons[i][j].setEnabled(false);
-                    } else {
-                        buttons[i][j].setText("X");
+                    } else if (counts[i][j] == 0) {
+                        buttons[i][j].setText(counts[i][j] + "");
                         buttons[i][j].setEnabled(false);
+
+                        ArrayList<Integer> massive = new ArrayList<>();
+                        massive.add(i * 100 + j);
+                        clearZeroes(massive);
+
                     }
+                } else {
+                    buttons[i][j].setText("X");
+                    buttons[i][j].setEnabled(false);
                 }
             }
+        }
+    }
+
+
+    private void clearZeroes(ArrayList<Integer> toClear) {
+        if (toClear.size() == 0) {
+            return;
+        } else {
+            int x = toClear.get(0) / 100;
+            int y = toClear.get(0) % 100;
+
+            toClear.remove(0);
+            if (counts[x][y] == 0) {
+
+                if (x > 0 && y > 0) {  //up left
+                    buttons[x - 1][y - 1].setText(counts[x - 1][y - 1] + "");
+                    buttons[x - 1][y - 1].setEnabled(false);
+                    if (counts[x - 1][y - 1] == 0) {
+                        toClear.add((x - 1) * 100 + (y - 1));
+                    }
+                }
+
+                if (y > 0) {  //up
+                    buttons[x][y - 1].setText(counts[x][y - 1] + "");
+                    buttons[x][y - 1].setEnabled(false);
+                    if (counts[x][y - 1] == 0) {
+                        toClear.add((x) * 100 + (y - 1));
+                    }
+                }
+
+                if (x < counts.length - 1 && y > 0) {  //up right
+                    buttons[x + 1][y - 1].setText(counts[x + 1][y - 1] + "");
+                    buttons[x + 1][y - 1].setEnabled(false);
+                    if (counts[x + 1][y - 1] == 0) {
+                        toClear.add((x + 1) * 100 + (y - 1));
+                    }
+                }
+
+                if (x > 0) {  // left
+                    buttons[x - 1][y].setText(counts[x - 1][y] + "");
+                    buttons[x - 1][y].setEnabled(false);
+                    if (counts[x - 1][y] == 0) {
+                        toClear.add((x - 1) * 100 + (y));
+                    }
+                }
+
+                if (x < counts.length - 1) {
+                    buttons[x + 1][y].setText(counts[x + 1][y] + "");
+                    buttons[x + 1][y].setEnabled(false);
+                    if (counts[x + 1][y] == 0) {
+                        toClear.add((x + 1) * 100 + y);
+                    }
+                }
+
+                if (x > 0 && y < counts[0].length - 1) {  //down left
+                    buttons[x - 1][y + 1].setText(counts[x - 1][y + 1] + "");
+                    buttons[x - 1][y + 1].setEnabled(false);
+                    if (counts[x - 1][y + 1] == 0) {
+                        toClear.add((x - 1) * 100 + (y + 1));
+                    }
+                }
+
+                if (y < counts[0].length - 1) {  //down
+                    buttons[x][y - 1].setText(counts[x][y - 1] + "");
+                    buttons[x][y - 1].setEnabled(false);
+                    if (counts[x][y - 1] == 0) {
+                        toClear.add((x) * 100 + (y - 1));
+                    }
+                }
+
+                if (x < counts.length - 1 && y < counts[0].length - 1) { //down right
+                    buttons[x + 1][y + 1].setText(counts[x + 1][y + 1] + "");
+                    buttons[x + 1][y + 1].setEnabled(false);
+                    if (counts[x + 1][y + 1] == 0) {
+                        toClear.add((x + 1) * 100 + (y + 1));
+                    }
+
+                }
+            }
+            clearZeroes(toClear);
         }
     }
 
